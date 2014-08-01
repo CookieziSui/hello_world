7620c62774a7cde15f84f0fbbe92ca2de8aa04bf
SuiKuo <yimeierskkk@126.com>
2014-08-01 21:58:45 +0800

    no message

--- hello_world.php
+++ hello_world.php
@@ -1,3 +1,18 @@
 <?php
 	echo "hello world";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cookiezi Login</title>
+</head>
+<body>
+<table>
+	<thead>
+	</thead>
+	<tbody>
+	</tbody>
+</table>
+</body>
+</html>
